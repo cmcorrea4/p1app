4d0a0d8a69c3a2a83cec54834635ec0483d1ee70
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-09-04 10:20:00 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -3,8 +3,13 @@ import streamlit as st
 st.title('Página 1')
 
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+import streamlit as st
+import pandas as pd
+#from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+ 
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
