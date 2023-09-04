34772b1ced3a836b3f348b588bc770df5c4241a0
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-09-04 10:37:39 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -13,3 +13,5 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+    st.subheader('Estadisticos Básicos')
+    st.write(dataframe.describe())
