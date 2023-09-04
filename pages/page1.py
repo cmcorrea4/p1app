963fc49e3deb3f5d761adc5ef55a35f70f72ece5
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-09-04 10:51:06 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -8,14 +8,14 @@ import pandas as pd
 #from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
-column_names=["Temperatura","Humedad"]
+column_names=["Humedad","Temperatura"]
 if uploaded_file is not None:
  
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file, names=column_names)
   
     st.write(dataframe)
-    chart_data = pd.DataFrame(dataframe, columns=['Temp', 'Hum'])
+    chart_data = pd.DataFrame(dataframe, columns=['Humedad', 'Temperatura'])
     st.line_chart(chart_data)
     st.subheader('Estadisticos Básicos')
     st.write(dataframe.describe())
