b2ef8e82b38967d9485c26171c87e2b6e5cf4cc4
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-11-15 11:26:23 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -15,9 +15,6 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file, names=column_names)
   
-    #st.write(dataframe)
-    #chart_data = pd.DataFrame(dataframe, columns=['Humedad', 'Temperatura'])
-    #st.line_chart(chart_data)
     st.subheader('Estadisticos Básicos')
     st.write(dataframe.describe())
     st.write(len(dataframe))
