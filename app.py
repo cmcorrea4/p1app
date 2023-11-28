06ea0704a282f6ecbe0d7d2d28fa8c514ac12505
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-11-28 10:47:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.header("MEJORA DE LA PRODUCTIVIDAD A TRAVÉS DEL USO DE TECNOLOGÍAS")
 image = Image.open("Agriculture.png")
 st.image(image, caption="Tecnología para Productividad")
 
-st.write("El uso de tecnologías de la Industria 4.0 permitirá mejorar las condiciones'
+st.write('El uso de tecnologías de la Industria 4.0 permitirá mejorar las condiciones'
          'de trabajo en el campo, la calidad de los productos y su productividad.')
 
 
