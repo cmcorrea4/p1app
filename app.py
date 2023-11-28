f6567ac702fd5f2be99dffc4dfd5614d83b34f82
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-11-28 10:47:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+from PIL import Image
+
 
 st.title('Aplicaciones Tecnológicas para el Campo')
+
+st.header("MEJORA DE LA PRODUCTIVIDAD A TRAVÉS DEL USO DE TECNOLOGÍAS")
+
+
+image = Image.open("Agriculture.png")
+st.image(image, caption="Tecnología para Productividad")
+
+st.write("El uso de tecnologías de la Industria 4.0 permitirá mejorar las condiciones'
+         'de trabajo en el campo, la calidad de los productos y su productividad.')
+
+
