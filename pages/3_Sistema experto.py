5247f8f3893e6c050054b2714d3961a4652948b4
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-11-28 08:55:11 -0500

    Update 3_Sistema experto.py

--- pages/3_Sistema experto.py
+++ pages/3_Sistema experto.py
@@ -33,7 +33,7 @@ ke = st.text_input('Escribe la clave.')
 try:
     os.environ['OPENAI_API_KEY'] = ke
     
-    pdfFileObj = open('Ruway.pdf', 'rb')
+    pdfFileObj = open('Cultivo.pdf', 'rb')
      
     # creating a pdf reader object
     pdfReader = PyPDF2.PdfReader(pdfFileObj)
