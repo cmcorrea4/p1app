8abc40e4218d12ae351ea3bd66d764c99ebe8730
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-12-01 11:22:49 -0500

    Update 4_Realidades extendidas.py

--- pages/4_Realidades extendidas.py
+++ pages/4_Realidades extendidas.py
@@ -10,4 +10,4 @@ st.title("Realidades extendidas")
 
 #st.write("Para saber más acerca del proyeto, ingresa en el siguiente link")
 
-link_text = "[Más información](https://sites.google.com/view/proyectoqijtikal/inicio)"
+link_text = "[Más información](https://www.spatial.io/s/HUB-TRADE-2023-647d18b10551fa0adc8060c5?share=0)"
