577de4a37cf5728004dbeefc8f346c7dcfa9e616
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-12-01 11:20:54 -0500

    Update 4_Realidades extendidas.py

--- pages/4_Realidades extendidas.py
+++ pages/4_Realidades extendidas.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from PIL import Image
 
+st.title("Realidades extendidas")
+
+#st.header("Proyecto de cosplay original diseñado y desarrollado por la Universidad EAFIT")
+
+#image = Image.open("bocetación.png")
+#st.image(image, caption="Ruway")
+
+#st.write("Para saber más acerca del proyeto, ingresa en el siguiente link")
+
+link_text = "[Más información](https://sites.google.com/view/proyectoqijtikal/inicio)"
